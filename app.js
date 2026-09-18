1e5e2037160c40eb54e8c05dedf1ac4f5065ec3e
JeyCubb <jacobiain123@gmail.com>
2026-09-19 07:59:05 +0800

    Add Quarry location to crop and plant scheduler

--- app.js
+++ app.js
@@ -940,7 +940,7 @@ document.getElementById('form-crop').addEventListener('submit', (e) => {
   };
 
   let shouldAddFirst = true;
-  if ((location === 'Main Farm' || location === 'Rail road' || location === 'Railroad' || location === 'Train Station') && !canGrowInSeason(cropKey, harvestDate.season)) {
+  if ((location === 'Main Farm' || location === 'Rail road' || location === 'Railroad' || location === 'Train Station' || location === 'Quarry') && !canGrowInSeason(cropKey, harvestDate.season)) {
     shouldAddFirst = false;
   }
   if (shouldAddFirst) {
@@ -968,7 +968,7 @@ document.getElementById('form-crop').addEventListener('submit', (e) => {
         };
 
         let shouldAddRegrow = true;
-        if ((location === 'Main Farm' || location === 'Rail road' || location === 'Railroad' || location === 'Train Station') && !canGrowInSeason(cropKey, nextHarvestDate.season)) {
+        if ((location === 'Main Farm' || location === 'Rail road' || location === 'Railroad' || location === 'Train Station' || location === 'Quarry') && !canGrowInSeason(cropKey, nextHarvestDate.season)) {
           if (!crop.isTree) {
             // Normal crops die on the Main Farm when their season ends, stopping future years
             break;
